09f7a421de19d842824181667a065673f5c10d81
Erwin Rohde <erwin@rohde.nu>
2018-05-17 20:49:27 +0200

    First commit

---
+++ js/contentScript.js
@@ -0,0 +1,21 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.privateinternetaccess.com", true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState != 4 || xhr.status != 200) {
+    return;
+  };
+
+  var hidden = document.body.appendChild(document.createElement("div"));
+  hidden.style.display = 'none';
+  hidden.innerHTML = xhr.responseText.replace(/^.*?<div class="topbar">(.*?)<\/div>.*?$/s,"$1");
+
+  if (hidden.getElementsByClassName('not-secured').length === 1) {
+    var animation = document.body.appendChild(document.createElement("style"));
+    animation.innerHTML = "@keyframes fade { 0% { opacity: 1 visibility: visible; } 100% { opacity: 0; visibility: hidden; } }";
+
+    var warning = document.body.appendChild(document.createElement("div"));
+    warning.style.cssText = 'width: 100%; position: fixed; left: 0; top: 0; background-color: red; z-index: 2147483647; color: white; text-align: center; font-size: 18px; padding: 10px; animation: fade 1s linear forwards 2s;';
+    warning.innerHTML = "You are not protected by PIA!";
+  }
+};
+xhr.send();
