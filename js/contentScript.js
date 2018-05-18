bfb8459bfa90a8ec38d1ec7d9341003f943661d3
Erwin Rohde <erwin@rohde.nu>
2018-05-18 13:17:39 +0200

    Improved shadow

--- js/contentScript.js
+++ js/contentScript.js
@@ -13,7 +13,7 @@ xhr.onreadystatechange = function () {
     animation.innerHTML = "@keyframes fade { 0% { opacity: 1 visibility: visible; } 100% { opacity: 0; visibility: hidden; } }";
 
     var warning = document.body.appendChild(document.createElement("div"));
-    warning.style.cssText = 'width: 100%; position: fixed; left: 0; top: 0; background-color: red; z-index: 2147483647; color: white; text-align: center; font-size: 18px; padding: 10px; box-shadow: 0px 5px 5px 0px rgba(170,170,170,1); animation: fade 1s linear forwards 2s;';
+    warning.style.cssText = 'width: 100%; position: fixed; left: 0; top: 0; background-color: red; z-index: 2147483647; color: white; text-align: center; font-size: 18px; padding: 10px 0 7px 0; box-shadow: rgb(255, 0, 0) 0px 3px 3px 3px; animation: fade 1s linear forwards 2s;';
     warning.innerHTML = "You are not protected by PIA";
   }
 };
